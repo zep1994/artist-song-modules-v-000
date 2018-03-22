bfd9255bcb3bdeece13f8a41d892abdbfaca5c5b
Thomas Wesley Matlock <thomasmatlock1994@gmail.com>
2018-03-22 21:17:49 +0000

    automatically backed up by learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,18 +1,17 @@
- module Memorable
-  module ClassMethods
-    def reset_all
-      self.all.clear
+     module Memorable
+      module ClassMethods
+        def reset_all
+          self.all.clear
+        end
+     
+        def count
+          self.all.count
+        end
+      end
+     
+      module InstanceMethods
+        def initialize
+          self.class.all << self
+        end
+      end
     end
-
-    def count
-      self.all.count
-    end
-  end
-  
-  module InstanceMethods
-    def initialize
-      self.class.all << self
-    end
-  end
-end
-
