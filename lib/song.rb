268f23b58187d3f58eaba629c1f6349557223c9e
Thomas Wesley Matlock <thomasmatlock1994@gmail.com>
2018-03-22 21:08:34 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,7 +2,11 @@ require 'pry'
 
 class Song
   
-  
+  extend Memorable::ClassMethods
+  extend Findable::ClassMethods
+  include Memorable::InstanceMethods
+  include Paramable::InstanceMethods
+
   
   attr_accessor :name
   attr_reader :artist
